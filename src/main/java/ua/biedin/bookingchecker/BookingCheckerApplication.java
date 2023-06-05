760544b79d177aa5063d4eec26189f9b6612affa
Ihor Biedin <ihorbiedin@MacBook-Air-Ihor.local>
2023-06-05 23:51:49 +0300

    init

---
+++ src/main/java/ua/biedin/bookingchecker/BookingCheckerApplication.java
@@ -0,0 +1,13 @@
+package ua.biedin.bookingchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingCheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/biedin/bookingchecker/BookingCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.biedin.bookingchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
